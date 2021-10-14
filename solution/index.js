e0210da17c3825fd54b1d41ebb46cafc7f641a20
Maksim Kungurtsev <maxkung.1996@gmail.com>
2021-10-14 18:05:45 +0500

    test empty body

--- solution/index.js
+++ solution/index.js
@@ -1,25 +1,6 @@
 module.exports = function (Homework) {
-  // вспомогательные фукнции и т.д.
-
-  const promisify = (func) =>
-    new Promise((resolve) => {
-      func((result) => {
-        resolve(result);
-      });
-    });
-
   return async (array, fn, initialValue, cb) => {
-    // асинхронный reduce
-    const length = await promisify(array.length);
     let result = initialValue;
-    let i = 0;
-
-    while (await promisify(Homework.less.bind(null, i, length))) {
-      const curr = await promisify(array.get.bind(null, i));
-      result = await promisify(fn.bind(null, result, curr, i, array));
-      i = await promisify(Homework.add.bind(null, i, 1));
-    }
-
     cb(result);
   };
 };
