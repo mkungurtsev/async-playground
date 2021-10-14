1762b8f6c2ed0afcd7cbda5f20ad1e5721d3deff
Maksim Kungurtsev <maxkung.1996@gmail.com>
2021-10-14 19:02:48 +0500

    refactor

--- solution/index.js
+++ solution/index.js
@@ -1,46 +1,22 @@
 module.exports = function (Homework) {
-  function promisify(fn) {
-    return function (...args) {
-      return new Promise((resolve) => {
-        fn(...args, resolve);
-      });
-    };
-  }
+  const promisify = (func) => (...args) =>
+    new Promise((resolve) => func(...args, resolve));
+  const isLesser = promisify(Homework.less);
+  const add = promisify(Homework.add);
 
-  return (asyncArray, fn, initialValue, cb) => {
-    if (!(asyncArray instanceof Homework.AsyncArray)) {
-      throw new TypeError("array is not an instance of AsyncArray");
+  return async (array, fn, initialValue, cb) => {
+    const getLength = promisify(array.length);
+    const getValue = promisify(array.get);
+    const compute = promisify(fn);
+
+    const length = await getLength();
+    let result = initialValue;
+
+    for (let i = 0; await isLesser(i, length); i = await add(i, 1)) {
+      const curr = await getValue(i);
+      result = await compute(result, curr, i, array);
     }
 
-    if (typeof fn !== "function") {
-      throw new TypeError("fn is not a function");
-    }
-
-    const getLength = promisify(asyncArray.length);
-    const getItem = promisify(asyncArray.get);
-    const callback = promisify(fn);
-    const add = promisify(Homework.add);
-    const less = promisify(Homework.less);
-    const equal = promisify(Homework.equal);
-
-    async function run() {
-      const length = await getLength();
-
-      if (await equal(length, 0)) {
-        return initialValue;
-      }
-
-      let acc = initialValue === undefined ? await getItem(0) : initialValue;
-      let i = initialValue === undefined ? 1 : 0;
-
-      for (; await less(i, length); i = await add(i, 1)) {
-        const curr = await getItem(i);
-        acc = await callback(acc, curr, i, asyncArray);
-      }
-
-      return acc;
-    }
-
-    run().then(cb);
+    cb(result);
   };
 };
